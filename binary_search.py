71087dda21b4912226925175288e2c0a4479accd
BaseBenjamin13 <benmorgiewicz@gmail.com>
2022-10-15 00:10:36 -0400

    binary search 02

--- binary_search.py
+++ binary_search.py
@@ -17,4 +17,25 @@ def binary_search(sequence, item):
 sequence_a = [2,4,5,6,7,8,9,10,12,13,14]
 item_a = 12
 
-print(binary_search(sequence_a, item_a))
+# print(binary_search(sequence_a, item_a))
+
+
+# Binary Search Optimized 
+# return index, or index of where it would be.
+def searchInsert(nums, target): 
+    i=0
+    j=len(nums)-1;
+    while i < j:
+        mid = (i+j) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] > target:
+            j = mid -1;
+        else:
+            i = mid +1
+    if nums[i] >= target:
+        return i
+    else: 
+        return i+1
+print(searchInsert([1,3,5,6], 5))
+        
